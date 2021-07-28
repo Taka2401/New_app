65b46cc9be442870d5bcb1d1a65b6b21fc7f869d
Taka2401 <tkht2401@gmail.com>
2021-07-28 20:41:26 +0900

    ルーティング変更

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  devise_for :users
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
