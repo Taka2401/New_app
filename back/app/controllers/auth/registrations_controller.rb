422641e19d4f5e4965ee1664912bcdd2827abaa2
Taka2401 <tkht2401@gmail.com>
2021-07-29 00:30:12 +0900

    fix: コントローラー修正

--- back/app/controllers/auth/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class Auth::RegistrationsController < DeviseTokenAuth::ApplicationController
-  private
-
-  def sign_up_params
-    params.permit(:name, :email, :password, :password_confirmation)
-  end
-end
